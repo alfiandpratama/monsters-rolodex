7573a2a15f8c3b8e7dc0e3eb8b806eee9146ccda
alfiandpratama <it.prohospital@gmail.com>
2019-07-31 17:59:42 +0700

    Updates

---
+++ precache-manifest.46a71783a1eb8aaba96c9dfc8a61e138.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31fa90168a01b7828532e23190e9b1ba",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c828342a918278b1e26e",
+    "url": "/monsters-rolodex/static/css/main.20d8175a.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "c828342a918278b1e26e",
+    "url": "/monsters-rolodex/static/js/main.19a8527b.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
